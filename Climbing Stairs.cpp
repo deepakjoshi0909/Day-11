6c3d9438effb6181dd0f94bd9c50ecd3687a797d
Deepak  Joshi <dj753233@gmail.com>
2024-08-02 22:17:40 +0530

    Create Climbing Stairs.cpp

---
+++ Climbing Stairs.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1){
+            return 1;
+        }
+
+        if(n == 2){
+            return 2;
+        }
+
+        int p1 = 1;
+        int p2 = 2;
+
+        for(int i=3;i<=n;i++){
+            int curr = p1 + p2;
+            p1 = p2;
+            p2 = curr;
+        }
+
+        return p2;
+    }
+};
